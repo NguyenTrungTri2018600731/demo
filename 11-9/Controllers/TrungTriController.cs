9f07062aafde48c2d2ec28ea09af7a7a67b1e513
NguyenTrungTri2018600731 <nguyentrungtri09@gmail.com>
2021-11-27 17:43:29 +0700

    them trung tri controller

---
+++ 11-9/Controllers/TrungTriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _11_9.Controllers
+{
+    public class TrungTriController : Controller
+    {
+        // GET: TrungTri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
